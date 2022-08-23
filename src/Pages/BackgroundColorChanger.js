a686c7a298d85c5ac69848ce94da8a8d30c2ab6a
Sachin kumar <95627047+Sachin7368@users.noreply.github.com>
2022-08-24 03:18:00 +0530

    Update BackgroundColorChanger.js

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -6,16 +6,18 @@ export const BackgroundColorChanger = () =>{
     const colors = ['red', 'cyan','green','yellow']
 
     return(
-        <div>
+        <> 
             {
-        colors.map(color=> {
+        colors.map(colorname=> {
             return (
-            <div id="main" style={{ height: "100vh", backgroundColor: `${color}`}}><Link to={`/bgcolor/${color}`}>Background Color Changer</Link></div>
+                <div id="main" style={{ height: "100vh", backgroundColor: `${colorname}`}}>
+                    <Link to={`/bgcolor/${colorname}`}>Background Color Changer</Link>
+                </div>
                 )
             }  
     )
 }
-</div>
+</>
     )
 }
  {/* // <div id="main" style={{backgroundColor: "red" , height: "100vh"}}>Background Color Changer</div> */}
