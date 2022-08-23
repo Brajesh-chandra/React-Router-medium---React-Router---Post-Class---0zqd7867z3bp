8dcf826b5779707b32271a786ee5658090c14a29
Sachin kumar <95627047+Sachin7368@users.noreply.github.com>
2022-08-24 02:53:08 +0530

    main

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import {Link, Routes, Route} from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 import { BackgroundColorChanger } from './Pages/BackgroundColorChanger'
 // import { Calculator } from './Pages/Calculator'
-import  {Index } from './Pages/Index'
+import { Index } from './Pages/Index'
 
-export const AppRoutes = () =>{
+export const AppRoutes = () => {
     return (
         <div>
-        <ul>
-        <li><Link to="/" element={<Index></Index>}>Index</Link></li>
-        {/* <Link To="/calculator" element={<Calculator></Calculator>}></Link> */}
-        <li><Link to="/bgcolor/:colorname" element={<BackgroundColorChanger></BackgroundColorChanger>}>Background Color Changer</Link></li>
-        </ul>
-        <Routes>
-            <Route path="/" element={<Index></Index>}></Route>
-            {/* <Route path="/calculator" element={<Calculator></Calculator>}></Route> */}
-            <Route path="/bgcolor/:colorname" element={<BackgroundColorChanger></BackgroundColorChanger>}></Route>
-        </Routes>
+            <ul>
+                <li><Link to="/">Index</Link></li>
+                <li><Link to="/bgcolor/red">Background Color Changer</Link></li>
+            </ul>
+            <Routes>
+                <Route path="/" element={<Index></Index>}></Route>
+                <Route path="/bgcolor/:colorname" element={<BackgroundColorChanger></BackgroundColorChanger>}></Route>
+            </Routes>
         </div>
     )
 }
--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import {useParams, Link} from 'react-router-dom';
 
 export const BackgroundColorChanger = () =>{
+    
+    const colors = ['red', 'cyan','green','yellow']
 
     return(
-
-        <div id="main" style={{backgroundColor: "red" , height: "100vh"}}>Background Color Changer</div>
+        <div>
+            {
+        colors.map(color=> {
+            return (
+            <div id="main" style={{ height: "100vh", backgroundColor: `${color}`}}><Link to={`/bgcolor/${color}`}>Background Color Changer</Link></div>
+                )
+            }  
     )
 }
+</div>
+    )
+}
+ {/* // <div id="main" style={{backgroundColor: "red" , height: "100vh"}}>Background Color Changer</div> */}
